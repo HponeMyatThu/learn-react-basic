4c1893e5361173d8620240b4d01a906fffb3683f
HponeMyatThu <hponemy8tthu@gmail.com>
2023-10-30 23:37:47 +0630

    [ Prototype ] testing

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component} from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Person.module.css';
 import Auxilitary from '../../../hoc/Auxilitary';
@@ -22,5 +23,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func,
+} 
 
 export default withClass(Person, classes.Person);
